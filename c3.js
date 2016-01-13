69f90a48b5c9d53680f9216c749d352143247ff8
Tiago Ferreira <tiago.ferreira@mindera.com>
2016-01-13 15:42:36 +0000

    last build

--- c3.js
+++ c3.js
@@ -5766,7 +5766,7 @@
         return [min, max];
     };
     c3_chart_internal_fn.updateZoom = function () {
-        var $$ = this, z = $$.config.zoom_enabled ? $$.zoom : function () {};
+        var $$ = this, z = $$.config.zoom_enabled && $$.config.zoom_type === 'scroll' ? $$.zoom : function () {};
         $$.main.select('.' + CLASS.zoomRect).call(z).on("dblclick.zoom", null);
         $$.main.selectAll('.' + CLASS.eventRect).call(z).on("dblclick.zoom", null);
     };
